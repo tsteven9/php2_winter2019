fe1ef8c78c84a4e220bf760e7d20e303d8b7cb20
Erica Stroude <e_stroud@live.concordia.ca>
2019-02-06 21:49:37 -0500

    ES: My MVC app - Lab 1.

---
+++ Lab1/StroudeErica/erica/js/myjs.js
@@ -0,0 +1,23 @@
+alert("Hello World!");
+
+      //   $("#button").click(function() {
+      //
+      // document.body.style.backgroundColor = "#ED6353";
+      //
+      //   document.getElementById("image").style.backgroundImage = "url('images/Kanchenjunga.jpg')";
+      //  document.getElementById("image").style.backgroundRepeat = "no-repeat";
+      //   document.getElementById("image").style.backgroundPosition = "bottom";
+      //   document.getElementById("image").style.backgroundSize = "cover";
+      //
+        //  document.getElementsById("sun").style.visibility = "visible";
+      //
+      //
+      //   document.getElementById("button").style.display = "none";
+      //
+      //   document.getElementById("sources").style.visibility = "visible";
+      //
+      //  });
+
+
+
+
